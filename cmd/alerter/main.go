b5463e251c80c9eca303fe52fd93a447df437262
maxim-programmer <koyushevmax@gmail.com>
2025-12-12 20:51:01 +0300

    feat: add database work to main.go

--- cmd/alerter/main.go
+++ cmd/alerter/main.go
@@ -2,27 +2,35 @@ package main
 
 import (
 	"context"
+	"cryptopricealerter/internal/models"
 	"cryptopricealerter/internal/pricefetcher"
+	"cryptopricealerter/internal/repository"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
 	var (
 		time_tick int
-		coins     string
 		api_key   string = os.Getenv("API_KEY")
+		user = os.Getenv("POSTGRES_USER")
+		password = os.Getenv("POSTGRES_PASSWORD")
+		dbname = os.Getenv("POSTGRES_DB")
+		host = os.Getenv("DB_HOST")
+		port = os.Getenv("DB_PORT")
 	)
 	prices := make(map[string]pricefetcher.Price)
 	fetcher := pricefetcher.NewFetcher()
 
 	fmt.Print("Введите время в секундах для интервала отправки запросов: ")
 	_, _ = fmt.Scan(&time_tick)
-	fmt.Print("Введите названия криптовалюты (если их несколько - через запятую, без пробела): ")
-	_, _ = fmt.Scan(&coins)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	done := make(chan bool, 1)
@@ -33,11 +41,37 @@ func main() {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT)
 
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.AutoMigrate(&models.Alert{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	alertRepo := repository.NewAlertRepository(db)
+
+	newAlert := &models.Alert{
+		Symbol: "bitcoin",
+		Condition: ">",
+		Price: 8000,
+	}
+
+	err = alertRepo.Create(newAlert)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Created new alert, ID =", newAlert.ID)
+
+
 	go func() {
 		<-sigs
 		done <- true
 	}()
-
 	for {
 		select {
 		case <-done:
@@ -45,13 +79,36 @@ func main() {
 			fmt.Println("\nПрограмма завершила работу!")
 
 		case <-ticker.C:
-			prices, err := fetcher.GetPrices(ctx, coins, prices, api_key)
+			alerts, err := alertRepo.GetAll()
 			if err != nil {
-				fmt.Println("Error:", err)
+				log.Fatal(err)
 			}
-			for i, v := range prices {
-				fmt.Println(i, v.USD)
+			for _, alert := range alerts {
+				if !alert.Triggered {
+					prices, err := fetcher.GetPrices(ctx, alert.Symbol, prices, api_key)
+					if err != nil {
+						log.Fatal("Error:", err)
+					}
+					for _, v := range prices {
+						switch alert.Condition {
+						case ">":
+							if v.USD > alert.Price {
+								_ = alertRepo.MarkTriggered(alert.ID)
+								fmt.Println("Триггер отработал!")
+							}
+						case "<":
+							if v.USD < alert.Price {
+								_ = alertRepo.MarkTriggered(alert.ID)
+								fmt.Println("Триггер отработал!")
+							}
+						}
+					}
+				} else {
+					_ = alertRepo.Delete(alert.ID)
+					fmt.Println("Алерт удален!")
+				}
 			}
+			
 
 		case <-ctx.Done():
 			return
